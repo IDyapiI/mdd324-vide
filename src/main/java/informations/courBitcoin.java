bf22f577d22b38b262804dc5e4b86896bc39cbf1
martLeveMart <mart.leveque69@gmail.com>
2018-12-19 14:34:27 +0100

    créer package informations

---
+++ src/main/java/informations/courBitcoin.java
@@ -0,0 +1,9 @@
+package informations;
+
+public class courBitcoin {
+    private double EUR;
+    private double USD;
+    private double GBP;
+    private double JPY;
+    private double AUD;
+}
---
+++ src/main/java/informations/infoRep.java
@@ -0,0 +1,7 @@
+package informations;
+
+public class infoRep {
+    private double bitcoinAmount;
+    private String currencyList;
+
+}
