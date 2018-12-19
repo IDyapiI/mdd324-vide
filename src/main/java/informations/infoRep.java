211a683ed15bf2c52402553e395e5593c6d8b61c
martLeveMart <mart.leveque69@gmail.com>
2018-12-19 14:42:41 +0100

    mise à jour des classe

--- src/main/java/informations/infoRep.java
+++ src/main/java/informations/infoRep.java
@@ -3,5 +3,30 @@ package informations;
 public class infoRep {
     private double bitcoinAmount;
     private String currencyList;
+    private courBitcoin currenciesEquivalent;
+
+    public double getBitcoinAmount() {
+        return bitcoinAmount;
+    }
+
+    public void setBitcoinAmount(double bitcoinAmount) {
+        this.bitcoinAmount = bitcoinAmount;
+    }
+
+    public String getCurrencyList() {
+        return currencyList;
+    }
+
+    public void setCurrencyList(String currencyList) {
+        this.currencyList = currencyList;
+    }
+
+    public courBitcoin getCurrenciesEquivalent() {
+        return currenciesEquivalent;
+    }
+
+    public void setCurrenciesEquivalent(courBitcoin currenciesEquivalent) {
+        this.currenciesEquivalent = currenciesEquivalent;
+    }
 
 }
